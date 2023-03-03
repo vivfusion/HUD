7e510284f535bd86c806fe384c4ae11ed6d9201d
“viv <vivfusion@outlook.com>
2023-03-03 11:25:20 -0500

    latest commi

---
+++ index.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="database_connection.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            Student Id :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TxtId" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Student name :&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TxtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Student class :&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TxtClass" runat="server" ></asp:TextBox>
+            <br />
+            <br />
+            Student subject :&nbsp;
+            <asp:TextBox ID="TxtSub" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Width="201px" Height="124px" style="margin-top: 0px"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Button ID="saveBtn" runat="server" Text="Save" OnClick="saveBtn_Click" />
+            <asp:Button ID="dispBtn" runat="server" Text="Display" OnClick="dispBtn_Click" />
+          
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace database_connection
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["stdKeyId"].ConnectionString;
+            if(con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        protected void saveBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert student_table values(@stdid,@stdname,@stdclass,@stdsub)";
+            cmd.Connection = con;
+            cmd.Parameters.AddWithValue("@stdid", TxtId.Text);
+            cmd.Parameters.AddWithValue("@stdname", TxtName.Text);
+            cmd.Parameters.AddWithValue("@stdclass", TxtClass.Text);
+            cmd.Parameters.AddWithValue("@stdsub", TxtSub.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            TxtId.Text = string.Empty;
+            TxtName.Text = string.Empty;
+            TxtClass.Text = string.Empty;
+            TxtSub.Text = string.Empty;
+            TxtId.Focus();
+            Display_Refactor();
+           
+        }
+
+        protected void dispBtn_Click(object sender, EventArgs e)
+        {
+            Display_Refactor();
+        }
+
+        private void  Display_Refactor()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from student_table";
+            cmd.Connection = con;
+            SqlDataReader stdDataReader;
+            stdDataReader = cmd.ExecuteReader();
+            ListBox1.DataTextField = "stdName";
+            ListBox1.DataValueField = "stdId";
+            ListBox1.DataSource = stdDataReader;
+            ListBox1.DataBind();
+            stdDataReader.Close();
+            cmd.Dispose();
+        }
+    }
+}
